3ed9bd7d1d15e1b74100b393e661dff1e3a91e33
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-10-25 15:41:33 +0600

    Fake Data Ready done

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
+const courses = require("./fakedata/courses.json");
+const coursesDeatails = require("./fakedata/coursesdetails.json");
+
 app.get("/", (req, res) => {
   res.send("Hello World with ashraful islam");
+});
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourses = coursesDeatails.find(
+    (course) => (course.coursesid = id)
+  );
+  res.send(selectedCourses);
 });
 app.listen(port, () => {
   console.log(`Port is running Succesfully 😍😍😍😍 on ${port}`);
