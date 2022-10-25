2ed75e7c84bb637a53f71d9f2f8eb1b1cefb58b2
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-10-25 16:13:25 +0600

    commmet added

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const port = process.env.PORT || 5000;
-app.use(cors());
-const courses = require("./fakedata/courses.json");
-const coursesDeatails = require("./fakedata/coursesdetails.json");
+const cors = require("cors"); // cors require kore newa holo
+const port = process.env.PORT || 5000; // hoi 5000 port kinva onno kono port e ta run korbe
+app.use(cors()); // client site data access korar jonnne use kora holo locally
+const courses = require("./fakedata/courses.json"); //// fake data variable set kora holo
+
+const coursesDeatails = require("./fakedata/coursesdetails.json"); // fake data variable set kora holo
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!Developer"); // "Defult route wellcome show korbe"
 });
 app.get("/courses", (req, res) => {
-  res.send(courses);
+  res.send(courses); // shokol dornoer course Show korbe
 });
 
 app.get("/courses/:id", (req, res) => {
   const id = req.params.id;
   const selectedCourses = coursesDeatails.find(
-    (course) => (course.coursesid = id)
+    (course) => (course.coursesid = id) // jodi Id ar courses id mileh jai tahole shei id related course details show korbe
   );
   res.send(selectedCourses);
 });
 app.listen(port, () => {
-  console.log(`Port is running Succesfully 😍😍😍😍 on ${port}`);
+  console.log(`Port is running Succesfully 😍😍😍😍 on ${port}`); // koto number porta theke run hobe server ta show korbe
 });
